7f6344c1d59a799bc3c0ffaa43f7f9a08f802a41
Max Bezditko <mb.preply.1@gmail.com>
2024-07-28 19:57:47 +0300

    change contracts, modules, entities

--- libs/modules/src/lib/rmq.module.ts
+++ libs/modules/src/lib/rmq.module.ts
@@ -1,31 +1,38 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module, DynamicModule, Global } from '@nestjs/common';
-import { ClientsModule } from '@nestjs/microservices';
 import {
-  getRmqConfig,
-  RmqConfigParams,
-} from '@taskfusion-microservices/helpers';
-
-type RmqDynamicModuleParams = RmqConfigParams & {
-  name: string;
-};
+  GENERAL_EXCHANGE_NAME,
+  USERS_QUEUE_NAME,
+  USERS_QUEUE_ROUTING_KEYS,
+} from '@taskfusion-microservices/constants';
 
 @Global()
 @Module({})
 export class RmqDynamicModule {
-  static register(config: RmqDynamicModuleParams): DynamicModule {
-    const { name, queue, queueOptions, replyQueue } = config;
-
+  static register(): DynamicModule {
     return {
       module: RmqDynamicModule,
       imports: [
-        ClientsModule.register([
-          {
-            name,
-            ...getRmqConfig({ queue, queueOptions, replyQueue }),
-          },
-        ]),
+        RabbitMQModule.forRoot(RabbitMQModule, {
+          uri: 'amqp://admin:admin@localhost:5672',
+          exchanges: [
+            {
+              name: GENERAL_EXCHANGE_NAME,
+              type: 'topic',
+              createExchangeIfNotExists: true,
+            },
+          ],
+          queues: [
+            {
+              name: USERS_QUEUE_NAME,
+              exchange: GENERAL_EXCHANGE_NAME,
+              routingKey: USERS_QUEUE_ROUTING_KEYS,
+              createQueueIfNotExists: true,
+            },
+          ],
+        }),
       ],
-      exports: [ClientsModule],
+      exports: [RabbitMQModule],
     };
   }
 }
