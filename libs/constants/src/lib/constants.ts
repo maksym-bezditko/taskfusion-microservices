e5d6e7e88ff1977bb80e164e128f03eb4156f55e
Max Bezditko <mb.preply.1@gmail.com>
2024-07-28 19:54:45 +0300

    add constants, helpers, types

---
+++ libs/constants/src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const GENERAL_EXCHANGE_NAME = 'taskfusion';
+
+export const DL_EXCHANGE_NAME = 'deadletter';
+
+export const USERS_QUEUE_NAME = `${GENERAL_EXCHANGE_NAME}.users`;
+
+export const DL_QUEUE_NAME = `${DL_EXCHANGE_NAME}.messages`;
+
+export const USERS_QUEUE_ROUTING_KEYS = `${USERS_QUEUE_NAME}.*`;
+
+export const DL_QUEUE_ROUTING_KEYS = `${DL_QUEUE_NAME}.*`;
+
+export const DL_UNIVERSAL_ROUTING_KEY = `${DL_QUEUE_NAME}.universal`;
