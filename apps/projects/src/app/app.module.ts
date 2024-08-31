51ca033d246a6986fd816d65523e2f116b6177b4
Max Bezditko <mb.preply.1@gmail.com>
2024-08-31 23:47:55 +0300

    implement rpc handlers for dev invites

--- apps/projects/src/app/app.module.ts
+++ apps/projects/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { InviteEntity, ProjectEntity } from '@taskfusion-microservices/entities';
+import {
+  PmInviteEntity,
+  DeveloperInviteEntity,
+  ProjectEntity,
+} from '@taskfusion-microservices/entities';
 import { getTypeOrmConfig } from '@taskfusion-microservices/helpers';
 import { RmqDynamicModule } from '@taskfusion-microservices/modules';
 import { InvitesModule } from './invites/invites.module';
@@ -18,7 +22,11 @@ import { InvitesModule } from './invites/invites.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) =>
-        getTypeOrmConfig(configService, [ProjectEntity, InviteEntity]),
+        getTypeOrmConfig(configService, [
+          ProjectEntity,
+          PmInviteEntity,
+          DeveloperInviteEntity,
+        ]),
     }),
     TypeOrmModule.forFeature([ProjectEntity]),
     RmqDynamicModule.register(),
--- apps/projects/src/app/invites/invites.service.ts
+++ apps/projects/src/app/invites/invites.service.ts
@@ -11,18 +11,22 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import {
+  AcceptDeveloperInviteContract,
   AcceptPmInviteContract,
   AssignUserToProjectContract,
   CheckUserContract,
-  GetInviteByIdContract,
+  GetPmInviteByIdContract,
   GetUserByEmailContract,
   GetUserByIdContract,
+  InviteDeveloperContract,
   InvitePmContract,
+  RejectDeveloperInviteContract,
   RejectPmInviteContract,
   SendEmailContract,
 } from '@taskfusion-microservices/contracts';
 import {
-  InviteEntity,
+  PmInviteEntity,
+  DeveloperInviteEntity,
   InviteStatus,
   ProjectParticipantRole,
   UserType,
@@ -34,8 +38,10 @@ import { handleRpcRequest } from '@taskfusion-microservices/helpers';
 @Injectable()
 export class InvitesService {
   constructor(
-    @InjectRepository(InviteEntity)
-    private readonly inviteEntityRepositoty: Repository<InviteEntity>,
+    @InjectRepository(PmInviteEntity)
+    private readonly pmInviteEntityRepositoty: Repository<PmInviteEntity>,
+    @InjectRepository(DeveloperInviteEntity)
+    private readonly developerInviteEntityRepositoty: Repository<DeveloperInviteEntity>,
     private readonly appService: AppService,
     private readonly amqpConnection: AmqpConnection
   ) {}
@@ -102,7 +108,7 @@ export class InvitesService {
       throw new Error('Client user not found');
     }
 
-    const existingInvite = await this.inviteEntityRepositoty.findOne({
+    const existingInvite = await this.pmInviteEntityRepositoty.findOne({
       where: {
         clientUserId,
         pmUserId: pmUser.id,
@@ -114,7 +120,7 @@ export class InvitesService {
       throw new Error('Invite already exists');
     }
 
-    const invite = await this.inviteEntityRepositoty.save({
+    const invite = await this.pmInviteEntityRepositoty.save({
       clientUserId,
       pmUserId: pmUser.id,
       project: project,
@@ -167,7 +173,7 @@ export class InvitesService {
     allowNonJsonMessages: true,
     name: 'accept-pm-invite',
   })
-  async acceptInvite(
+  async acceptPmInvite(
     dto: AcceptPmInviteContract.Dto
   ): Promise<AcceptPmInviteContract.Response> {
     const { inviteId, pmUserId } = dto;
@@ -190,7 +196,7 @@ export class InvitesService {
       }
     );
 
-    const invite = await this.inviteEntityRepositoty.findOne({
+    const invite = await this.pmInviteEntityRepositoty.findOne({
       where: {
         id: inviteId,
       },
@@ -209,7 +215,7 @@ export class InvitesService {
       throw new BadRequestException('Invite is not valid anymore');
     }
 
-    await this.inviteEntityRepositoty.update(
+    await this.pmInviteEntityRepositoty.update(
       {
         id: inviteId,
       },
@@ -266,7 +272,7 @@ export class InvitesService {
       }
     );
 
-    const invite = await this.inviteEntityRepositoty.findOne({
+    const invite = await this.pmInviteEntityRepositoty.findOne({
       where: {
         id: inviteId,
       },
@@ -285,7 +291,7 @@ export class InvitesService {
       throw new BadRequestException('Invite is not valid anymore');
     }
 
-    await this.inviteEntityRepositoty.update(
+    await this.pmInviteEntityRepositoty.update(
       {
         id: inviteId,
       },
@@ -301,26 +307,284 @@ export class InvitesService {
   }
 
   @RabbitRPC({
-    exchange: GetInviteByIdContract.exchange,
-    routingKey: GetInviteByIdContract.routingKey,
-    queue: GetInviteByIdContract.queue,
+    exchange: GetPmInviteByIdContract.exchange,
+    routingKey: GetPmInviteByIdContract.routingKey,
+    queue: GetPmInviteByIdContract.queue,
     errorBehavior: MessageHandlerErrorBehavior.NACK,
     errorHandler: defaultNackErrorHandler,
     allowNonJsonMessages: true,
-    name: 'get-invite-by-id',
+    name: 'get-pm-invite-by-id',
   })
-  async getInviteById(
-    dto: GetInviteByIdContract.Dto
-  ): Promise<GetInviteByIdContract.Response> {
+  async getPmInviteById(
+    dto: GetPmInviteByIdContract.Dto
+  ): Promise<GetPmInviteByIdContract.Response> {
     const { id } = dto;
 
-    const invite = await this.inviteEntityRepositoty.findOne({
+    const pmInvite = await this.pmInviteEntityRepositoty.findOne({
       where: {
         id,
       },
       relations: ['project'],
     });
 
-    return invite;
+    return pmInvite;
+  }
+
+  @RabbitRPC({
+    exchange: InviteDeveloperContract.exchange,
+    routingKey: InviteDeveloperContract.routingKey,
+    queue: InviteDeveloperContract.queue,
+    errorBehavior: MessageHandlerErrorBehavior.NACK,
+    errorHandler: defaultNackErrorHandler,
+    allowNonJsonMessages: true,
+    name: 'invite-developer',
+  })
+  async inviteDeveloper(
+    dto: InviteDeveloperContract.Dto
+  ): Promise<InviteDeveloperContract.Response> {
+    const { pmUserId, email, projectId } = dto;
+
+    const project = await this.appService.getProjectById({
+      projectId,
+    });
+
+    // check if project is assigned to PM
+
+    if (!project) {
+      throw new Error('Project not found');
+    }
+
+    const developerUserResult =
+      await this.amqpConnection.request<GetUserByEmailContract.Response>({
+        exchange: GetUserByEmailContract.exchange,
+        routingKey: GetUserByEmailContract.routingKey,
+        payload: {
+          email,
+        } as GetUserByEmailContract.Request,
+      });
+
+    const developerUser = await handleRpcRequest(
+      developerUserResult,
+      async (response) => response
+    );
+
+    if (!developerUser || developerUser.userType !== UserType.DEVELOPER) {
+      throw new Error('Developer user not found');
+    }
+
+    const pmUserResult =
+      await this.amqpConnection.request<GetUserByIdContract.Response>({
+        exchange: GetUserByIdContract.exchange,
+        routingKey: GetUserByIdContract.routingKey,
+        payload: {
+          id: pmUserId,
+        } as GetUserByIdContract.Request,
+      });
+
+    const pmUser = await handleRpcRequest(
+      pmUserResult,
+      async (response) => response
+    );
+
+    if (!pmUser || pmUser.userType !== UserType.PM) {
+      throw new Error('Pm user not found');
+    }
+
+    const existingInvite = await this.developerInviteEntityRepositoty.findOne({
+      where: {
+        pmUserId: pmUser.id,
+        projectId: project.id,
+        developerUserId: developerUser.id,
+      },
+    });
+
+    if (existingInvite) {
+      throw new Error('Invite already exists');
+    }
+
+    const invite = await this.developerInviteEntityRepositoty.save({
+      developerUserId: developerUser.id,
+      pmUserId: pmUser.id,
+      project: project,
+      isActive: true,
+      expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 1), // 1 day
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await this.amqpConnection.publish(
+      SendEmailContract.exchange,
+      SendEmailContract.routingKey,
+      {
+        recipientEmail: developerUser.email,
+        subject: 'Project Invitation from TaskFusion',
+        message: `
+          <div style="font-family: Arial, sans-serif; color: #333; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
+            <h2 style="color: #4CAF50; text-align: center;">Project Invitation</h2>
+            <p>Hi,</p>
+            <p>You have been invited to join the project <strong>${project.title}</strong> by <strong>${pmUser.name}</strong> (${pmUser.email}).</p>
+            <p style="text-align: center;">
+              <a href="http://localhost:8000/developer/project-invitation/${invite.id}" 
+                style="display: inline-block; padding: 10px 20px; color: white; background-color: #4CAF50; border-radius: 5px; text-decoration: none; font-weight: bold;">
+                Accept Invitation
+              </a>
+            </p>
+            <p>If the button above doesn’t work, please click on the link below or copy and paste it into your browser:</p>
+            <p style="word-break: break-all;">
+              <a href="http://localhost:8000/developer/project-invitation/${invite.id}" style="color: #4CAF50;">
+                http://localhost:8000/developer/project-invitation/${invite.id}
+              </a>
+            </p>
+            <p>Thank you!</p>
+          </div>
+        `,
+      } as SendEmailContract.Dto
+    );
+
+    return {
+      id: invite.id,
+    };
+  }
+
+  @RabbitRPC({
+    exchange: AcceptDeveloperInviteContract.exchange,
+    routingKey: AcceptDeveloperInviteContract.routingKey,
+    queue: AcceptDeveloperInviteContract.queue,
+    errorBehavior: MessageHandlerErrorBehavior.NACK,
+    errorHandler: defaultNackErrorHandler,
+    allowNonJsonMessages: true,
+    name: 'accept-developer-invite',
+  })
+  async acceptDeveloperInvite(
+    dto: AcceptDeveloperInviteContract.Dto
+  ): Promise<AcceptDeveloperInviteContract.Response> {
+    const { inviteId, developerUserId } = dto;
+
+    const userResult =
+      await this.amqpConnection.request<CheckUserContract.Response>({
+        exchange: CheckUserContract.exchange,
+        routingKey: CheckUserContract.routingKey,
+        payload: {
+          userId: developerUserId,
+        } as CheckUserContract.Request,
+      });
+
+    await handleRpcRequest<CheckUserContract.Response>(
+      userResult,
+      async (response) => {
+        if (!response.exists) {
+          throw new NotFoundException('User not found!');
+        }
+      }
+    );
+
+    const invite = await this.developerInviteEntityRepositoty.findOne({
+      where: {
+        id: inviteId,
+      },
+    });
+
+    if (!invite) {
+      throw new NotFoundException('Invite not found');
+    }
+
+    const isInviteValid =
+      new Date(invite.expiresAt) > new Date() &&
+      invite.inviteStatus === InviteStatus.PENDING &&
+      invite.developerUserId === developerUserId;
+
+    if (!isInviteValid) {
+      throw new BadRequestException('Invite is not valid anymore');
+    }
+
+    await this.developerInviteEntityRepositoty.update(
+      {
+        id: inviteId,
+      },
+      {
+        inviteStatus: InviteStatus.ACCEPTED,
+        updatedAt: new Date(),
+      }
+    );
+
+    await this.amqpConnection.request<AssignUserToProjectContract.Response>({
+      exchange: AssignUserToProjectContract.exchange,
+      routingKey: AssignUserToProjectContract.routingKey,
+      payload: {
+        projectId: invite.projectId,
+        userId: developerUserId,
+        role: ProjectParticipantRole.DEVELOPER,
+      } as AssignUserToProjectContract.Request,
+    });
+
+    return {
+      success: true,
+    };
+  }
+
+  @RabbitRPC({
+    exchange: RejectDeveloperInviteContract.exchange,
+    routingKey: RejectDeveloperInviteContract.routingKey,
+    queue: RejectDeveloperInviteContract.queue,
+    errorBehavior: MessageHandlerErrorBehavior.NACK,
+    errorHandler: defaultNackErrorHandler,
+    allowNonJsonMessages: true,
+    name: 'reject-developer-invite',
+  })
+  async rejectDeveloperInvite(
+    dto: RejectDeveloperInviteContract.Dto
+  ): Promise<RejectDeveloperInviteContract.Response> {
+    const { inviteId, developerUserId } = dto;
+
+    const userResult =
+      await this.amqpConnection.request<CheckUserContract.Response>({
+        exchange: CheckUserContract.exchange,
+        routingKey: CheckUserContract.routingKey,
+        payload: {
+          userId: developerUserId,
+        } as CheckUserContract.Request,
+      });
+
+    await handleRpcRequest<CheckUserContract.Response>(
+      userResult,
+      async (response) => {
+        if (!response.exists) {
+          throw new NotFoundException('User not found!');
+        }
+      }
+    );
+
+    const invite = await this.developerInviteEntityRepositoty.findOne({
+      where: {
+        id: inviteId,
+      },
+    });
+
+    if (!invite) {
+      throw new NotFoundException('Invite not found');
+    }
+
+    const isInviteValid =
+      new Date(invite.expiresAt) > new Date() &&
+      invite.inviteStatus === InviteStatus.PENDING &&
+      invite.pmUserId === developerUserId;
+
+    if (!isInviteValid) {
+      throw new BadRequestException('Invite is not valid anymore');
+    }
+
+    await this.developerInviteEntityRepositoty.update(
+      {
+        id: inviteId,
+      },
+      {
+        inviteStatus: InviteStatus.REJECTED,
+        updatedAt: new Date(),
+      }
+    );
+
+    return {
+      success: true,
+    };
   }
 }
