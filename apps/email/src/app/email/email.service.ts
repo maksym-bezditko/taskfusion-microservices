0ef8ca30163998a4cdcf5af161eabd33062b4427
Max Bezditko <mb.preply.1@gmail.com>
2024-09-18 11:23:08 +0300

    add logging and custom error handling for email and payment microservices

--- apps/email/src/app/email/email.service.ts
+++ apps/email/src/app/email/email.service.ts
@@ -2,11 +2,17 @@ import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { BaseService } from '@taskfusion-microservices/common';
 import { SendEmailContract } from '@taskfusion-microservices/contracts';
 
 @Injectable()
-export class EmailService {
-	constructor(private readonly mailService: MailerService, private readonly configService: ConfigService) {}
+export class EmailService extends BaseService {
+  constructor(
+    private readonly mailService: MailerService,
+    private readonly configService: ConfigService
+  ) {
+    super(EmailService.name);
+  }
 
   @RabbitSubscribe({
     exchange: SendEmailContract.exchange,
@@ -27,5 +33,7 @@ export class EmailService {
         address: this.configService.getOrThrow<string>('EMAIL_USERNAME'),
       },
     });
+
+    this.logger.log(`Email sent to ${recipientEmail}`);
   }
 }
