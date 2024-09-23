1cc9ab01f0bd71ff273a0ff062cf7cb6f476e870
Max Bezditko <mb.preply.1@gmail.com>
2024-09-23 20:08:51 +0300

    update payment entities

--- libs/entities/src/index.ts
+++ libs/entities/src/index.ts
@@ -3,7 +3,7 @@ export * from './lib/client.entity';
 export * from './lib/project.entity';
 export * from './lib/developer.entity';
 export * from './lib/pm.entity';
-export * from './lib/payment.entity';
+export * from './lib/payment-request.entity';
 export * from './lib/task.entity';
 export * from './lib/comment.entity';
 export * from './lib/tasksUsers.entity';
