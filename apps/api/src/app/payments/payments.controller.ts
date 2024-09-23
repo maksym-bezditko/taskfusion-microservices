47cd1d9a8ddce1bbc284d6d1b1cce323f46847ee
Max Bezditko <mb.preply.1@gmail.com>
2024-09-23 20:10:44 +0300

    implement endpoints for payment requests

--- apps/api/src/app/payments/payments.controller.ts
+++ apps/api/src/app/payments/payments.controller.ts
@@ -1,23 +1,61 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import {
   AtJwtGuard,
+  ClientGuard,
   CustomAmqpConnection,
+  PmGuard,
 } from '@taskfusion-microservices/common';
 import {
+  AcceptPaymentRequestContract,
   CreateCheckoutSessionContract,
+  CreatePaymentRequestContract,
+  RejectPaymentRequestContract,
 } from '@taskfusion-microservices/contracts';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly customAmqpConnection: CustomAmqpConnection) {}
 
-  @UseGuards(AtJwtGuard)
+  @UseGuards(AtJwtGuard, ClientGuard)
   @Post('create-checkout-session')
   async createCheckoutSession(
     @Body() dto: CreateCheckoutSessionContract.Request
   ): Promise<CreateCheckoutSessionContract.Response> {
     return this.customAmqpConnection.requestOrThrow<CreateCheckoutSessionContract.Response>(
       CreateCheckoutSessionContract.routingKey,
+      dto
+    );
+  }
+
+  @UseGuards(AtJwtGuard, PmGuard)
+  @Post('create-payment-request')
+  async createPaymentRequest(
+    @Body() dto: CreatePaymentRequestContract.Request
+  ): Promise<CreatePaymentRequestContract.Response> {
+    return this.customAmqpConnection.requestOrThrow<CreatePaymentRequestContract.Response>(
+      CreatePaymentRequestContract.routingKey,
+      dto
+    );
+  }
+
+  @UseGuards(AtJwtGuard, ClientGuard)
+  @Post('accept-payment-request')
+  async acceptPaymentRequest(
+    @Body() dto: AcceptPaymentRequestContract.Request
+  ): Promise<AcceptPaymentRequestContract.Response> {
+    return this.customAmqpConnection.requestOrThrow<AcceptPaymentRequestContract.Response>(
+      AcceptPaymentRequestContract.routingKey,
+      dto
+    );
+  }
+
+  @UseGuards(AtJwtGuard, ClientGuard)
+  @Post('reject-payment-request')
+  async rejectPaymentRequest(
+    @Body() dto: RejectPaymentRequestContract.Request
+  ): Promise<RejectPaymentRequestContract.Response> {
+    return this.customAmqpConnection.requestOrThrow<RejectPaymentRequestContract.Response>(
+      RejectPaymentRequestContract.routingKey,
       dto
     );
   }
