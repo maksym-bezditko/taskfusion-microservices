f3935067177d8751cfa3c3d7dd8ba560cee0efa3
Max Bezditko <mb.preply.1@gmail.com>
2024-10-20 14:03:12 +0300

    migrate rmq connection string to env variables

--- libs/modules/src/lib/rmq.module.ts
+++ libs/modules/src/lib/rmq.module.ts
@@ -1,5 +1,6 @@
-import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { RabbitMQConfig, RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module, DynamicModule, Global } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CustomAmqpConnection } from '@taskfusion-microservices/common';
 import { GENERAL_EXCHANGE_NAME } from '@taskfusion-microservices/constants';
 
@@ -10,15 +11,32 @@ export class RmqDynamicModule {
     return {
       module: RmqDynamicModule,
       imports: [
-        RabbitMQModule.forRoot(RabbitMQModule, {
-          uri: 'amqp://admin:admin@rabbitmq-service:5672',
-          exchanges: [
-            {
-              name: GENERAL_EXCHANGE_NAME,
-              type: 'topic',
-              createExchangeIfNotExists: true,
-            },
-          ],
+        ConfigModule,
+        RabbitMQModule.forRootAsync(RabbitMQModule, {
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (
+            configService: ConfigService
+          ): RabbitMQConfig | Promise<RabbitMQConfig> => {
+            const user = configService.getOrThrow<string>('RABBITMQ_USER');
+            const password =
+              configService.getOrThrow<string>('RABBITMQ_PASSWORD');
+            const host = configService.getOrThrow<string>('RABBITMQ_HOST');
+            const port = configService.getOrThrow<string>('RABBITMQ_PORT');
+
+            const uri = `amqp://${user}:${password}@${host}:${port}`;
+
+            return {
+              uri,
+              exchanges: [
+                {
+                  name: GENERAL_EXCHANGE_NAME,
+                  type: 'topic',
+                  createExchangeIfNotExists: true,
+                },
+              ],
+            };
+          },
         }),
       ],
       providers: [
